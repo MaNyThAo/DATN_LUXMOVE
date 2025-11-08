a0726f0738ac8fd23512ceb3c74b9a3d408b9d11
MaNyThAo <thaotienhihi111@gmail.com>
2025-11-08 11:27:37 +0700

    Update one day

--- DATN_LUXMOVE/js/components/header.js
+++ DATN_LUXMOVE/js/components/header.js
@@ -1,14 +1,4 @@
-// js/main.js
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("./assets/components/header.html")
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("Không thể tải header.html");
-      }
-      return response.text();
-    })
-    .then(data => {
-      document.getElementById("header").innerHTML = data;
-    })
-    .catch(error => console.error(error));
-});
+fetch("./assets/components/header.html")
+  .then(res => res.text())
+  .then(data => document.getElementById("header").innerHTML = data)
+  .catch(err => console.error("Lỗi header:", err));
